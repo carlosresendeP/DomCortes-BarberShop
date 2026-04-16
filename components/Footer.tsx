fddacaf9cd26c4862fd38a1e2a11763ae0a18e8a
carlosresendeP <carlosresende.paula@gmail.com>
2026-04-16 17:30:06 -0300

    feat: correção no alinhamento do footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-background py-12 border-t border-zinc-900">
       <div className="max-w-7xl mx-auto px-6">
-        <div className="flex flex-col md:flex-row justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between items-start">
           <div className="flex flex-col items-start gap-2">
             <span className="text-primary font-black text-xl">Dom Cortês</span>
             <p className="text-zinc-500 text-sm mt-4 md:mt-0">
